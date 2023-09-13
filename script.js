0458b0f3ff9b194b742c3e1428c2ef9eaec0ed94
devjonasbt <jonas.bcruz@senacsp.edu.br>
2023-09-12 22:03:51 -0300

    mudei varias coisas

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function menuMobile(){
     });
 
 }
+var images = [
+    "assets/past-pet/pet1.jpg",
+    "assets/past-pet/pet2.webp",
+    "assets/past-pet/pet3.jpg",
+    "assets/past-pet/pet4.jpg"
+];
+var cont=0;
+function backImg(){
+   var bg = document.getElementById('bg');
+   var index = Math.floor(Math.random()*images.length);
+   bg.style.backgroundImage='url('+images[index]+')';
+}
+setInterval(backImg, 3000);
