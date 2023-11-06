24a744a24a4c41539dd7bb22a8adfc33b598ca7b
Danilo Rocha <contato_dsr@hotmail.com>
2023-11-06 16:01:31 -0300

    projeto sem persistencia

---
+++ src/main/java/br/com/socialpet/servlet/Cadastro.java
@@ -0,0 +1,22 @@
+package br.com.socialpet.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-cadastro")
+public class Cadastro extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String nome = request.getParameter("nome");
+        System.out.println(nome);
+        request.getRequestDispatcher("index.html").forward(request,response);
+
+    }
+}
